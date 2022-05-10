5c9e1461c4510055421d5fe3af443e7c7a00f56d
saanvi <71885805+Saanvi-21@users.noreply.github.com>
2022-05-10 18:51:56 +0530

    Add files via upload

---
+++ remote_Server.py
@@ -0,0 +1,64 @@
+from http import client
+from ipaddress import ip_address
+import socket
+from  threading import Thread
+from tkinter import EXCEPTION
+from pynput.mouse import Button, Controller
+from screeninfo import get_monitors
+from pynput.keyboard import Key, Controller
+
+from remote_keyboard import IP_ADDRESS, SERVER
+
+SERVER = None
+PORT = 8000
+IP_ADDRESS = "192.168.0.1"
+screen_width = None
+screen_height = None
+
+keyboard = Controller()
+
+def getDeviceSize():
+    global screen_width
+    global screen_height
+
+    for m in get_monitors():
+        screen_width = int(str(m).split(",")[2].strip().split('width=')[1])
+        screen_width = int(str(m).split(",")[3].strip().split('height=')[1])
+
+def recvMessage(client_socket):
+    global keyboard
+    while True:
+        try:
+            message = client_socket.recv(2048).decode()
+            if (message):
+                keyboard.press(message)
+                keyboard.release(message)
+                print(message)
+
+        except Exception as error:
+            pass
+
+def acceptConections():
+    global SERVER
+    while True:
+        client_socket, addr = SERVER.accept()
+        print(f"Connection established with {client_socket} : {addr}")
+        thread1 = Thread(target = recvMessage, args = (client_socket,))
+        thread1.start()
+
+def setup():
+    print("\n\t\t\t\t\t*** Welcome to Remote Mouse *** \n")
+    
+    global SERVER
+    global PORT
+    global IP_ADDRESS
+
+    SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    SERVER.bind((IP_ADDRESS, PORT))
+    SERVER.listen(10)
+
+    print("\t\t\t\tSERVER IS WAITING FOR INCOMMUNG CONNECTIONS...\n")
+    getDeviceSize()
+    acceptConections()
+
+setup()
